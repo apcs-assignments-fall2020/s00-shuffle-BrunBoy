e20c9141b026254266410d50874e09eb915535ca
BrunBoy <23rreddy@athenian.org>
2021-01-08 17:57:03 +0000

    done

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,12 +2,32 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int[] smeerkat=new int[arr.length/2];
+        for (int i=0; i<arr.length/2; i++){
+            smeerkat[i]=arr[i];
+        }
+        int[] mykat=new int[arr.length/2];
+        for (int j=arr.length/2; j<arr.length; j++){
+            mykat[j-arr.length/2]=arr[j];
+        }
+        int[] result=new int[arr.length];
+        for (int a=0; a<arr.length; a+=2){
+            result[a]=smeerkat[a/2];
+            result[a+1]=mykat[a/2];
+        }
+        return result;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
-    public static int[] selectionShuffle(int[] arr) { 
-        return null;
+    public static int[] selectionShuffle(int[] arr){
+        int woww=0;
+        for (int i=0; i<arr.length; i++){
+            int nope= (int)(Math.random()*(arr.length));
+            woww=arr[i];
+            arr[i]=arr[nope];
+            arr[nope]=woww;
+        }
+        return arr;
     }
 
 
